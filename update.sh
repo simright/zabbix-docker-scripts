61302c34f5b0d8c5bca158ae6f0c82b64e7bc8d3
terrynie <88@terrynie.com>
2017-09-12 11:25:18 +0800

    create update shell

---
+++ update.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo 'UserParameter=inventory[*],sudo /bin/bash /home/ubuntu/simright/zabbix/scripts/inventory.py $1 $2 $3' >> /etc/zabbix/zabbix_agentd.conf
+sudo kill $(awk '{print $2}' <(sudo ps aux | grep "zabbix_agentd$"))
+sudo zabbix_agentd
